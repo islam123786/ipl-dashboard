0f50162c5cac5c3ccc911ce8a73fc87be58d0b2a
Siddiqui <MS021480@cerner.net>
2021-10-07 02:46:44 +0530

    Added Match Page to React App

--- src/frontend/src/pages/MatchPage.js
+++ src/frontend/src/pages/MatchPage.js
@@ -1,9 +1,26 @@
-import { React } from 'react';
+import { React, useEffect, useState } from 'react';
+import { MatchDetailCard } from '../components/MatchDetailCard';
+import { useParams } from 'react-router-dom';
 
 export const MatchPage = () => {
+
+    const [matches, setMatches] = useState([]);
+    const {teamName, year} = useParams();
+
+    useEffect(() => {
+        const fetchMatches =  async () => {
+            const response = await fetch(`http://localhost:9090/team/${teamName}/matches?year=${year}`);
+            const data = await response.json();
+            setMatches(data);
+            console.log(data);
+          };
+          fetchMatches();
+        },[]
+      );
+
   return (
     <div className="MatchPage">
-      <h1>Match Page</h1>
+      {matches.map(match => <MatchDetailCard teamName={teamName} match={match} /> )}
     </div>
   );
 }
